5388dcd316982d4bcc5c3cd6296c6bf7facd9c7f
Yuliia <julia.kostandi@gmail.com>
2024-12-12 07:51:50 +0200

    Четверте завдання

--- Exercises/4-difference.js
+++ Exercises/4-difference.js
@@ -3,6 +3,8 @@
 // Find difference of two arrays
 // elements from array1 that are not includes in array2
 
-const difference = (array1, array2) => [];
+const difference = (array1, array2) => {
+    return array1.filter(item => !array2.includes(item));
+};
 
 module.exports = { difference };
