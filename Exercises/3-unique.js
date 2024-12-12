0476cff53f6f59fc02efd23b2aacbae2f2845466
Yuliia <julia.kostandi@gmail.com>
2024-12-12 07:28:40 +0200

    Третє завдання

--- Exercises/3-unique.js
+++ Exercises/3-unique.js
@@ -3,6 +3,14 @@
 // Create and return a new array without duplicate elements
 // Don't modify initial array
 
-const unique = (array) => [];
+const unique = (array) => {
+    const result = [];
+    for (const element of array) {
+        if (!result.includes(element)) {
+            result.push(element);
+        }
+    }
+    return result;
+};
 
 module.exports = { unique };
