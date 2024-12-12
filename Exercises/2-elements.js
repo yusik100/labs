154fe486998b416227b677a8b61a5b49eb2d52f3
Yuliia <julia.kostandi@gmail.com>
2024-12-12 07:10:40 +0200

    Друге завдання

--- Exercises/2-elements.js
+++ Exercises/2-elements.js
@@ -1,7 +1,12 @@
 'use strict';
 
 const removeElements = (array, ...items) => {
-  // Remove multiple items from array modifying original array
+  for (const item of items) {
+    const indexOfItem = array.indexOf(item);
+    if (indexOfItem !== -1) {
+      array.splice(indexOfItem, 1);
+    }
+  }
 };
 
 module.exports = { removeElements };
